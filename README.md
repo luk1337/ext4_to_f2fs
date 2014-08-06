c71a963912b6ef13acd3cdb109235410ede35742
LuK1337 <priv.luk@gmail.com>
2014-08-07 00:15:09 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+GitHub Markup
+=============
+This is a simple tool for converting EXT4 roms to F2FS.
+
+Compatability
+-----
+* Paranoid Android (Since 4.5-BETA1)
+
+Usage
+-----
+Linux:
+    cd linux
+    chmod +x convert.sh
+    ./convert.sh [ROM_NAME].zip
---
+++ linux/convert.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+ROM="$1"
+
+if [ -z "$ROM" ]; then
+	echo "[ERROR] Zip name is empty!"; exit
+fi
+
+chmod +x tools/*
+
+# Unzipping rom
+unzip "$ROM" -d ".rom"
+
+# Patching updater script
+patch -p0 < patches/updater-script.patch
+
+# Patching ramdisk
+mv .rom/boot.img .
+tools/split_boot boot.img
+patch -p0 < patches/fstab.patch
+patch -p0 < patches/init.capri.patch
+
+# Downloading F2FS binaries
+wget https://raw.githubusercontent.com/SamsungBCM-Cyanogenmod/android_device_samsung_s2vep/9efea0c9dcb970c1ca9ab3b6c1a38d115fe76b33/ramdisk/sbin/automount -O boot/ramdisk/sbin/automount
+wget https://github.com/SamsungBCM-Cyanogenmod/android_device_samsung_s2vep/blob/9efea0c9dcb970c1ca9ab3b6c1a38d115fe76b33/ramdisk/sbin/busybox?raw=true -O boot/ramdisk/sbin/busybox
+
+# Repacking ramdisk
+cd boot
+../tools/repack_ramdisk ramdisk boot.img-ramdisk.cpio.gz
+cd ..
+
+# Repacking boot image
+tools/mkbootimg --kernel boot/boot.img-kernel --ramdisk boot/boot.img-ramdisk.cpio.gz --pagesize 4096 --base 0xa2000000 --ramdiskaddr 0xa3000000 --cmdline "console=ttyS0,115200n8 mem=832M@0xA2000000 androidboot.console=ttyS0 vc-cma-mem=0/176M@0xCB000000" -o .rom/boot.img
+
+# Repacking rom file
+cd .rom
+zip -r ../"F2FS-$ROM" .
+cd ..
+
+# Cleaning
+rm -rf .rom boot
+
+echo "Done! ROM File: F2FS-$ROM"
