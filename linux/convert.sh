f8a1e5c99c0cba52302683e85aa372ad9c1fd0f5
LuK <priv.luk@gmail.com>
2014-08-07 16:20:58 +0200

    Add support for Cygwin

--- linux/convert.sh
+++ linux/convert.sh
@@ -17,6 +17,7 @@ patch -p0 < patches/updater-script.patch
 # Patching ramdisk
 mv .rom/boot.img .
 tools/split_boot boot.img
+rm -rf boot.img
 if [ -f "boot/ramdisk/fstab.capri_ss_s2ve" ]; then #s2ve
 	cat patches/fstab.patch | sed 's/s2vep/s2ve/g' | patch -p0
 	cat patches/init.capri.patch | sed 's/s2vep/s2ve/g' | patch -p0
@@ -35,7 +36,11 @@ cd boot
 cd ..
 
 # Repacking boot image
-tools/mkbootimg --kernel boot/boot.img-kernel --ramdisk boot/boot.img-ramdisk.cpio.gz --pagesize 4096 --base 0xa2000000 --ramdiskaddr 0xa3000000 --cmdline "console=ttyS0,115200n8 mem=832M@0xA2000000 androidboot.console=ttyS0 vc-cma-mem=0/176M@0xCB000000" -o .rom/boot.img
+if [ `uname -o` = "Cygwin" ]; then
+	tools/mkbootimg.exe --kernel boot/boot.img-kernel --ramdisk boot/boot.img-ramdisk.cpio.gz --pagesize 4096 --base 0xa2000000 --cmdline "console=ttyS0,115200n8 mem=832M@0xA2000000 androidboot.console=ttyS0 vc-cma-mem=0/176M@0xCB000000" -o .rom/boot.img
+else
+	tools/mkbootimg --kernel boot/boot.img-kernel --ramdisk boot/boot.img-ramdisk.cpio.gz --pagesize 4096 --base 0xa2000000 --ramdiskaddr 0xa3000000 --cmdline "console=ttyS0,115200n8 mem=832M@0xA2000000 androidboot.console=ttyS0 vc-cma-mem=0/176M@0xCB000000" -o .rom/boot.img
+fi
 
 # Repacking rom file
 cd .rom
